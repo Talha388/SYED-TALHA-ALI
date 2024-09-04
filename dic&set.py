b637430c70d8c58e3201dfa095a9103fe08872aa
Syed Talha Ali Shah <talhajani3636@gmail.com>
2024-09-04 10:43:49 +0500

    This is python

---
+++ dic&set.py
@@ -0,0 +1,32 @@
+
+myDic = {}
+
+item1 = input("Enter item1 : ")
+
+price1 = input("Enter price for {}:   ".format(item1))
+
+myDic[item1]=price1
+
+print(myDic)
+
+
+
+
+
+Set = set()
+
+num1= input("Enter number : ")
+Set[num1]
+num2= input("Enter number:  ")
+Set[num2]
+
+print(Set)
+
+
+
+
+
+
+
+
+
